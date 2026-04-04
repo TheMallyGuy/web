109b6a2cd067996e5bff4866a7f3a1c25f079b6b
Mal <minimal.spacing.a@gmail.com>
2026-04-04 16:53:55 +0700

    stuffs

---
+++ src/routes/stuff/+page.svelte
@@ -0,0 +1,13 @@
+<div class="w-170 h-76.75 mt-30 mx-auto flex flex-col flex-nowrap items-center content-stretch justify-start gap-6.5 px-60.5 py-20.75">
+    <h1 class="text-[72px] leading-none">
+        stuffs
+    </h1>
+    <p class="text-[16px] text-[#606060]">
+        my projects
+    </p>
+</div>
+<div class="w-170.25 h-77 mx-auto flex flex-row flex-nowrap items-center content-stretch justify-center px-62.25 py-33.25">
+    <p class="text-[16px] text-[#606060] text-center">
+        what?! there's nothing? maybe check my github...
+    </p>
+</div>

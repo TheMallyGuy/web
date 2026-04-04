bbe85eb5d2e6b159395fa70a956d59c0be2654b4
Mal <minimal.spacing.a@gmail.com>
2026-04-04 16:46:57 +0700

    mally

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-170.5 mx-auto mt-30 flex flex-col flex-nowrap items-start justify-start gap-6.5">
+
+    <!-- first card -->
+    <div class="w-full h-50.5 flex flex-col gap-2">
+        <h1 class="text-[72px] leading-none">
+            hello...
+        </h1>
+        <p class="text-[16px] text-[#606060]">
+            i'm mally, a new developer from vietnam
+        </p>
+    </div>
+
+    <!-- second card -->
+    <div class="w-full h-50.5 flex flex-col flex-nowrap items-end justify-center gap-2">
+        <p class="text-[16px] text-[#606060] text-right">
+            rust (mostly, tauri), typescript (no javascript lol), svelte, vietnamese and english!
+        </p>
+        <h1 class="text-[72px] leading-none">
+            i know....
+        </h1>
+    </div>
+
+    <!-- third card -->
+    <div class="w-full h-50.5 flex flex-col gap-2">
+        <h1 class="text-[72px] leading-none">
+            fun fact....
+        </h1>
+        <p class="text-[16px] text-[#606060]">
+            cats can meow
+        </p>
+    </div>
+</div>

acbc9fa4320fee1159917ab0ef403e272075fd1c
Mal <minimal.spacing.a@gmail.com>
2026-04-04 16:48:45 +0700

    fix warning

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
 	import './layout.css'
     import Navbar from '$lib/navbar.svelte';
+    let { children } = $props();
 </script>
 
 <div class="flex flex-col min-h-screen bg-black text-stone-200 overflow-x-hidden">
     <Navbar/>
-    <slot />
+    {@render children?.()}
 </div>

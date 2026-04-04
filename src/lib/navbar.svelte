c28666cda4e52bc7f10fa473144b6cdad9eeabd9
Mal <minimal.spacing.a@gmail.com>
2026-04-04 16:20:20 +0700

    update links

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,5 +1,5 @@
 <nav class="w-170.5 h-18.75 absolute left-1/2 -translate-x-1/2 top-0 bg-white/0 flex flex-nowrap items-center content-stretch justify-center gap-[39px]">
     <a href="/" class="text-[18px]">mally</a>
-    <a href="/" class="text-[18px]">stuff</a>
-    <a href="/" class="text-[18px]">donate</a>
+    <a href="/stuff" class="text-[18px]">stuff</a>
+    <a href="/donate" class="text-[18px]">donate</a>
 </nav>

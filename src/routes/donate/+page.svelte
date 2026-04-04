8fb01559afb32c54722bd79673560bba5ebcdc16
Mal <minimal.spacing.a@gmail.com>
2026-04-04 16:53:01 +0700

    donate

---
+++ src/routes/donate/+page.svelte
@@ -0,0 +1,25 @@
+<div class="w-[680px] mx-auto mt-30 flex flex-col flex-nowrap items-center content-stretch justify-start px-[242px] py-[83px]">
+    <h1 class="text-[72px] leading-none text-[#b2b2b2]">
+        donate
+    </h1>
+    <p class="text-[16px] text-[#606060]">
+        please support me
+    </p>
+</div>
+
+<div class="w-[681px] mx-auto flex flex-col flex-nowrap items-start content-stretch justify-start gap-[34px] px-[33px] py-[8px]">
+
+    <!-- crypto addresses -->
+    <div class="flex flex-col items-start gap-[10px]">
+        <p class="text-[24px] text-[#b2b2b2]">via crypto</p>
+        <p class="text-[18px] text-[#b2b2b2]">BTC : <span class="font-['Bricolage_Grotesque']">bc1q6320wmxeg6j2kzv8xyw0nhuy342620ww73nex9</span></p>
+        <p class="text-[18px] text-[#b2b2b2]">ETH: <span class="font-['Bricolage_Grotesque']">0xB6c370ada323D6C0D6A989D3383616104cc998a8</span></p>
+    </div>
+
+    <!-- other services -->
+    <div class="flex flex-col items-start gap-[25px]">
+        <p class="text-[24px] text-[#b2b2b2]">other services</p>
+        <p class="text-[18px] text-[#b2b2b2]">not avaliable, comeback when i open a bank account</p>
+    </div>
+
+</div>

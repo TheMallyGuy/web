8a8d6b47a0082e5880789b0efaab851e3352f576
Mal <minimal.spacing.a@gmail.com>
2026-04-04 16:53:51 +0700

    what does this even do

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-[680px] mx-auto mt-30 flex flex-col flex-nowrap items-center content-stretch justify-start px-[242px] py-[83px]">
+<div class="w-170 mx-auto mt-30 flex flex-col flex-nowrap items-center content-stretch justify-start px-60.5 py-20.75">
     <h1 class="text-[72px] leading-none text-[#b2b2b2]">
         donate
     </h1>
@@ -7,17 +7,17 @@
     </p>
 </div>
 
-<div class="w-[681px] mx-auto flex flex-col flex-nowrap items-start content-stretch justify-start gap-[34px] px-[33px] py-[8px]">
+<div class="w-170.25 mx-auto flex flex-col flex-nowrap items-start content-stretch justify-start gap-8.5 px-8.25 py-2">
 
     <!-- crypto addresses -->
-    <div class="flex flex-col items-start gap-[10px]">
+    <div class="flex flex-col items-start gap-2.5">
         <p class="text-[24px] text-[#b2b2b2]">via crypto</p>
         <p class="text-[18px] text-[#b2b2b2]">BTC : <span class="font-['Bricolage_Grotesque']">bc1q6320wmxeg6j2kzv8xyw0nhuy342620ww73nex9</span></p>
         <p class="text-[18px] text-[#b2b2b2]">ETH: <span class="font-['Bricolage_Grotesque']">0xB6c370ada323D6C0D6A989D3383616104cc998a8</span></p>
     </div>
 
     <!-- other services -->
-    <div class="flex flex-col items-start gap-[25px]">
+    <div class="flex flex-col items-start gap-6.25">
         <p class="text-[24px] text-[#b2b2b2]">other services</p>
         <p class="text-[18px] text-[#b2b2b2]">not avaliable, comeback when i open a bank account</p>
     </div>
